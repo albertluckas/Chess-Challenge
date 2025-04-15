a831eea083b6e3ecac5e1b30758af0a5831507fc
albertluckas <121692929+albertluckas@users.noreply.github.com>
2025-04-15 12:09:25 -0500

    seve some tokens i think?

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -7,48 +7,119 @@ namespace ChessChallenge.Example
     // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
-        public Move Think(Board board, Timer timer)
-        {
-            Move[] allMoves = board.GetLegalMoves();
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+    Boolean playingAsWhite;
+    public Move Think(Board board, Timer timer) //Returns the move to play
+    {
+        playingAsWhite = board.IsWhiteToMove; //Figure out what color pieces you are playing with
+        return chooseMove(board, 2);
+    }
 
-            foreach (Move move in allMoves)
-            {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
-                    break;
+    public int EvalBoard(Board board){ //Evaluates the current board state
+        int evalScore = 0;
+        if(board.IsInCheckmate()){ //If checkmate, add infinite value
+                return(board.IsWhiteToMove ? -99999 : 99999);
+            }
+        for(int i = 0; i < 64; i++){
+            Piece piece = board.GetPiece(new Square(i));
+            int pieceColorModifier = piece.IsWhite ? 1 : -1;
+            if(!piece.IsNull){
+                if(piece.IsPawn){ //Favors positions where pawns are closer to the other side of the board (and closer to promotion)
+                    if(pieceColorModifier == 1){
+                        evalScore += (i/8);
+                    }
+                    else{
+                        evalScore += (7 - (i/8))*pieceColorModifier;
+                    }
                 }
+                else{ //Favors positions where pieces (except for the king) have been moved from their starting position
+                    if(!piece.IsKing){
+                        if(pieceColorModifier == 1 && i > 7){
+                            evalScore += 2;
+                        }
+                        if(pieceColorModifier == -1 && i < 56){
+                            evalScore -= 2;
+                        }
+                        
+                    }
+                }
+                
+            }
+            if(piece.IsPawn){
+                evalScore += 100 * pieceColorModifier;
+                evalScore += (3 - Math.Min(Math.Abs((i%8) - 4), Math.Abs((i%8) - 3))) * pieceColorModifier; //Favors pawns in the center of the board
+            }
+            if(piece.IsBishop){
+                evalScore += 302 * pieceColorModifier;
+            }
+            if(piece.IsKnight){
+                evalScore += 300 * pieceColorModifier;
+            }
+            if(piece.IsRook){
+                evalScore += 500 * pieceColorModifier;
+            }
+            if(piece.IsQueen){
+                evalScore += 900 * pieceColorModifier;
+            }
+            if(piece.IsKing){
+                evalScore += 10000 * pieceColorModifier;
+            }
+            
+            if(board.IsInCheck()){ //Favors giving checks
+                evalScore += board.IsWhiteToMove ? -10 : 10;
+            }
+        }
+        return evalScore;
+    }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+    public Move chooseMove(Board board, int maxDepth){
+        Random rng = new();
+        Move[] moves = board.GetLegalMoves();
+        Move bestMove = moves[0];
 
-                if (capturedPieceValue > highestValueCapture)
-                {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+        int bestEval = playingAsWhite ? -99999 : 99999; 
+
+        foreach(Move move in moves){
+
+            board.MakeMove(move); //Tries the first move, then checks further outcomes
+            int moveEval = checkTree(board, maxDepth); //Activate recursion to check deeper
+            board.UndoMove(move); //Undoes move to reset for next move it checks
+
+            if(playingAsWhite ? (moveEval > bestEval) : (moveEval < bestEval)){ //For the person moving, find the best move they can make (most + for white, most - for black)
+                bestEval = moveEval;
+                bestMove = move;
+            }
+            else if(moveEval == bestEval){ //If two positions are evaluated the same, choose randomly between them
+                if(rng.Next(2) == 0){
+                    bestMove = move;
                 }
             }
 
-            return moveToPlay;
+        }
+        return bestMove;
+    }
+
+    public int checkTree(Board board, int depth){
+        if(depth == 0){ //Base Case
+            return EvalBoard(board);
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
-        {
+        Move[] moves = board.GetLegalMoves();
+
+        int bestEval = board.IsWhiteToMove ? -99999 : 99999;
+
+        foreach(Move move in moves){
+
             board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
+            int moveEval = checkTree(board, depth-1); //Continue checking, decrement depth
             board.UndoMove(move);
-            return isMate;
+
+            if(board.IsWhiteToMove ? (moveEval > bestEval) : (moveEval < bestEval)){ 
+                bestEval = moveEval;
+            }
+            
         }
+        return bestEval;
     }
-}
+}}
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,65 +1,106 @@
-﻿using System.Runtime;
-using ChessChallenge.API;
+﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
-    private static readonly int[] pieceValues=[0,1,3,3,5,7,99];
-    
-    
-    public Move Think(Board board, Timer timer)
+
+    Boolean playingAsWhite;
+    Move bestMove;
+    public Move Think(Board board, Timer timer) //Returns the move to play
     {
-        int amWhite=board.IsWhiteToMove?1:-1;
-        
-         Move[] moves = board.GetLegalMoves();
-        int bestMove=0;
-        int best=-1000000;
-        for (int i = 0; i < moves.Length; i++)
-        {
-            board.MakeMove(moves[i]);
-            
-            int e =MinMax(board,3,0)*amWhite;
-            if(e>best){
-                best=e;
-                bestMove=i;
+        playingAsWhite = board.IsWhiteToMove; //Figure out what color pieces you are playing with
+        return chooseMove(board, 2);
+    }
+
+    public int EvalBoard(Board board){ //Evaluates the current board state
+        if(board.IsInCheckmate()){ //If checkmate, add infinite value
+            return(board.IsWhiteToMove ? -99999 : 99999);
+        }
+        int evalScore = 0;
+        if(board.IsInCheck()){ //Favors giving checks
+            evalScore += board.IsWhiteToMove ? -50 : 50;
+        }
+        for(int i = 0; i < 64; i++){
+            Piece piece = board.GetPiece(new Square(i));
+            int pieceColorModifier = piece.IsWhite ? 1 : -1;
+            if(!piece.IsNull){
+                if(piece.IsPawn){ //Favors positions where pawns are closer to the other side of the board (and closer to promotion)
+                    if(pieceColorModifier == 1){
+                        evalScore += (i/8);
+                    }
+                    else{
+                        evalScore += (7 - (i/8))*pieceColorModifier;
+                    }
+                }
+                else{ //Favors positions where pieces (except for the king) have been moved from their starting position
+                    if(!piece.IsKing&&!piece.IsRook){
+                        if(pieceColorModifier == 1 && i > 7){
+                            evalScore += 2;
+                        }
+                        if(pieceColorModifier == -1 && i < 56){
+                            evalScore -= 2;
+                        }
+                        
+                    }
+                }
+                
             }
-            board.UndoMove(moves[i]);
+            if(piece.IsPawn){
+                evalScore += 100 * pieceColorModifier;
+                evalScore += (3 - Math.Min(Math.Abs((i%8) - 4), Math.Abs((i%8) - 3))) * pieceColorModifier; //Favors pawns in the center of the board
+            }
+            if(piece.IsBishop){
+                evalScore += 302 * pieceColorModifier;
+            }
+            if(piece.IsKnight){
+                evalScore += 300 * pieceColorModifier;
+            }
+            if(piece.IsRook){
+                evalScore += 500 * pieceColorModifier;
+            }
+            if(piece.IsQueen){
+                evalScore += 900 * pieceColorModifier;
+            }
+            if(piece.IsKing){
+                evalScore += 10000 * pieceColorModifier;
+            }
 
         }
-
-        return moves[bestMove];
+        return evalScore;
     }
-    private static int eval(Board board){
-        int total=0;
-        Piece p;
-        for (int i = 0; i < 64; i++)
-        {
-            p=board.GetPiece(new Square(i));
-            total+=pieceValues[(int)p.PieceType]* (p.IsWhite?1:-1);
-        }
-        return total;
 
-    }
-    private static int MinMax(Board board ,int d,int sign){
-        if(d==0){
-            return eval(board);
-        }
-        int amWhite=board.IsWhiteToMove?1:-1;
-        
+    public Move chooseMove(Board board, int maxDepth){
+        Random rng = new();
         Move[] moves = board.GetLegalMoves();
-        int best=-1000000;
-        for (int i = 0; i < moves.Length; i++)
-        {
-            board.MakeMove(moves[i]);
-            
-            int e =MinMax(board,d-1,-sign)*amWhite;
-            if(e>best){
-                best=e;
-            }
-            board.UndoMove(moves[i]);
+        bestMove = moves[0];
 
-        }
-        return best*amWhite;
+        int bestEval = playingAsWhite ? -99999 : 99999; 
 
+        checkTree(board,3,true);
+        return bestMove;
     }
 
+    public int checkTree(Board board, int depth, Boolean root){
+        if(depth == 0){ //Base Case
+            return EvalBoard(board);
+        }
+
+        Move[] moves = board.GetLegalMoves();
+
+        int bestEval = board.IsWhiteToMove ? -99999 : 99999;
+        foreach(Move move in moves){
+
+            board.MakeMove(move);
+            int moveEval = checkTree(board, depth-1,false); //Continue checking, decrement depth
+            board.UndoMove(move);
+
+            if(board.IsWhiteToMove ? (moveEval > bestEval) : (moveEval < bestEval)){ 
+                bestEval = moveEval;
+                if(root){bestMove = move;}
+            }
+            
+        }
+        
+        return bestEval;
+    }
 }
